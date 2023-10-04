41b24dfea51b193cfac0ea54a3836a6e1e913b99
Artogg <40455433+Artogg@users.noreply.github.com>
2023-10-04 12:50:08 +0200

    pyrenet.py

--- MyToolBox.py
+++ MyToolBox.py
@@ -1,1 +1,18 @@
+from netmiko import ConnectHandler
+from datetime import datetime
 
+def timestamp():
+    date = datetime.now()
+    return date = f'{date.day}-{date.month}-{date.year}'
+  
+def set_conf(routers, user, passwd, enable, config_file):
+    for router in routers:
+      try:
+          device = { 'device_type': 'cisco_ios', 'host':'router', 'username': user, 'password': passwd, 'secret': enable, 'verbose': True }
+          connection = ConnectHandler(**device)
+          connection.enable
+          output = connection.send_config_from_file(config_file)
+          date = timestamp()
+          with open(f'{router}-{date}.txt', 'w') as f:
+            f.write(output)
+          connection.disconnect()
