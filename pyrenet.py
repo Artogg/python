696181e5f85922b08a33929356da90046496c112
Artogg <40455433+Artogg@users.noreply.github.com>
2023-10-04 12:58:50 +0200

    Update pyrenet.py

--- pyrenet.py
+++ pyrenet.py
@@ -5,14 +5,18 @@ def timestamp():
     date = datetime.now()
     return date = f'{date.day}-{date.month}-{date.year}'
   
-def set_conf(routers, user, passwd, enable, config_file):
-    for router in routers:
-      try:
-          device = { 'device_type': 'cisco_ios', 'host':'router', 'username': user, 'password': passwd, 'secret': enable, 'verbose': True }
-          connection = ConnectHandler(**device)
-          connection.enable
-          output = connection.send_config_from_file(config_file)
-          date = timestamp()
-          with open(f'{router}-{date}.txt', 'w') as f:
-            f.write(output)
-          connection.disconnect()
+def set_conf(router_file, user, passwd, enable, config_file):
+    try:
+        with open(router_file, 'r') as f:
+            routers = f.read().splitlines()
+        router_list = list()
+        for router in routers:
+            device = { 'device_type': 'cisco_ios', 'host': router, 'username': user, 'password': passwd, 'secret': enable, 'verbose': True }
+            router_list.append(device)
+            connection = ConnectHandler(**device)
+            connection.enable
+            output = connection.send_config_from_file(config_file)
+            date = timestamp()
+            with open(f'{router}-{date}.txt', 'w') as f:
+                f.write(output)
+            connection.disconnect()
